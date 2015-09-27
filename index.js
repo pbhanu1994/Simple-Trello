0723407bcd026c4cce5254d6d619b30358b86bbf
Bhanu <pbhanu.1994@gmail.com>
2015-09-27 23:10:40 +0530

    changed 1

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-alert("Hello World!");
+function close_x(){
+	document.getElementById("note").style.display = 'none';
+}
+
+$("#addtask").keyup(function(e) {
+    if(e.which == 13) {
+        alert('You pressed enter!');
+	}
+});
